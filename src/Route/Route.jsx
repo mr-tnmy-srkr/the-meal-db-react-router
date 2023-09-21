3b38f0346affcc29b62f71345e2863e7fba4a069
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-21 23:05:21 +0530

    completed

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
 
-const Route = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
+import Meals from "../Pages/Meals/Meals";
+import DashboardLayout from "../Pages/DashboardLayout/DashboardLayout";
+import Meal from "../Pages/Meal/Meal";
+import ShowMealDetails from "../Pages/Meal/ShowMealDetails";
 
-export default Route;
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+          loader: () => fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=chicken`),
+        },
+        {
+          path: "/products",
+          element: <Meals></Meals>,
+          loader: () => fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`),
+        },
+        {
+          path: "/category/:strCategory",
+          element: <Meal></Meal>,
+          loader: ({params}) => fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${params.strCategory}`),
+        },
+        {
+          path: "/product/:id",
+          element: <ShowMealDetails></ShowMealDetails>,
+          // loader: ({params})=>fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
+        },
+        {
+          path: "/dashboard",
+          element: <DashboardLayout></DashboardLayout>,
+        },
+      ],
+    }
+  ]);
+  export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import MainLayout from "./Layouts/MainLayout";
-import Home from "./Pages/Home/Home";
-import Meals from "./Pages/Meals/Meals";
-import DashboardLayout from "./Pages/DashboardLayout/DashboardLayout";
-import Meal from "./Pages/Meal/Meal";
-import ShowMealDetails from "./Pages/Meal/ShowMealDetails";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-        loader: () => fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=chicken`),
-      },
-      {
-        path: "/products",
-        element: <Meals></Meals>,
-        loader: () => fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`),
-      },
-      {
-        path: "/category/:strCategory",
-        element: <Meal></Meal>,
-        loader: ({params}) => fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${params.strCategory}`),
-      },
-      {
-        path: "/product/:id",
-        element: <ShowMealDetails></ShowMealDetails>,
-        // loader: ({params})=>fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
-      },
-      {
-        path: "/dashboard",
-        element: <DashboardLayout></DashboardLayout>,
-      },
-    ],
-  }
-]);
+import router from "./Route/Route";
+import { RouterProvider } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
